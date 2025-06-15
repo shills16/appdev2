468c0516b7720c3d97fdbfcf0d8ca799cf3543cb
shills16 <shielamiclat10@gmail.com>
2025-06-15 19:39:04 +0800

    Initial commit

--- 07_laboratory_url_module.js/url.js
+++
@@ -1,7 +0,0 @@
-const url = require('url');
-
-const myURL = 'https://www.example.com:8080/pathname?name=JohnDoe#fragment';
-const parsedURL = url.parse(myURL,true);
-
-console.log(parsedURL);
-console.log('Parsed URL Object:', parsedURL.query.name);
